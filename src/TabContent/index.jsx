a58cb92d73be7fb19a0d48bf9dfca1acee630f3b
mayjoonll <mayjoonll@gmail.com>
2025-09-19 12:12:12 +0900

    d

--- src/TabContent/index.jsx
+++ src/TabContent/index.jsx
@@ -1,10 +1,35 @@
-function TabContent({tabState}){
+import { useEffect, useState } from "react";
+import DetailInfo from "../TabInfo/DetailInfo";
+import SizeGuide from "../TabInfo/SizeGuide";
+import Shipping from "../TabInfo/Shipping";
+
+function TabContent({tabState, id}){
+  // fade 값으로 css를 지정하도록 ...
+  let [fade, setFade] = useState('')
+
+  // useEffect로 타이머를 실행
+  // 0.2초 후에 fade = ani_end로 바꿔줌
+  useEffect(()=>{
+    let timer = setTimeout(() => {
+      setFade('ani_end')
+      }, 100);
+      return(()=>{
+        clearTimeout(timer);
+        setFade('')
+      })
+    }, [tabState]
+  )
+
+
   return(
-    [
-      <div>내용 1</div>,
-      <div>내용 2</div>,
-      <div>내용 3</div>,
-    ][tabState]
+    <div className={`ani_start ${fade}`}>
+      {
+        [
+          <DetailInfo id={id}/>,
+          <SizeGuide />,
+          <Shipping />,
+        ][tabState]}
+    </div>
   )
 }
 export default TabContent;

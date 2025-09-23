1aaa5b823f4208c9af7cc10e8f54f57659f29d17
mayjoonll <mayjoonll@gmail.com>
2025-09-23 14:54:52 +0900

    asd

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlS6_ucijBzmtC81Nx-jeex7p0_Uc2pkM",
+  authDomain: "react-shop-app-1b925.firebaseapp.com",
+  projectId: "react-shop-app-1b925",
+  storageBucket: "react-shop-app-1b925.firebasestorage.app",
+  messagingSenderId: "103808048373",
+  appId: "1:103808048373:web:ffe1df6272e0ee298139bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
